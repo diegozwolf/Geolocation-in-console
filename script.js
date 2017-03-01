d811ecc9117b1524c1e8f85a011f8572a73f9210
diegozwolf <diegozuluaga88@gmail.com>
2017-03-01 14:23:39 -0500

    add comments

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-
+//selecciona elsvg d ela brujula
 const arrow = document.querySelector('.arrow');
+//busca el span para agregar la velocidad
 const speed = document.querySelector('.speed-value');
-
+//búsqueda de coordenadas
 navigator.geolocation.watchPosition((data)=>{
 	console.log(data);
 	speed.textContent = data.coords.speed;
